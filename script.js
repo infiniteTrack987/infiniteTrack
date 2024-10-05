01c9c96ac7273ae18cfb15086f36eefaa72fde00
infiniteTrack987 <InfiniteTrack987@gmail.com>
2024-10-05 17:19:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 // script.js
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("Welcome to InfiniteTrack! Explore our content and subscribe.");
+    
+   const contactForm = document.getElementById('contact-form');
+   
+   contactForm.addEventListener('submit', function(event) {
+       event.preventDefault(); // Prevent default form submission
+       alert("Thank you for your message! We'll get back to you soon.");
+       contactForm.reset(); // Reset form fields after submission
+   });
+
+   console.log("Welcome to InfiniteTrack! Explore our content and subscribe.");
 });
