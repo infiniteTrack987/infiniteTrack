f806d02e0c01360d63de883c6e4667d5baa0ff85
infiniteTrack987 <InfiniteTrack987@gmail.com>
2024-10-14 17:14:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,20 +32,14 @@ function generateEpisodeButtons(season) {
     let buttons = '';
     for (let i = 1; i <= 52; i++) {
         let episodeNum = i < 10 ? '0' + i : i;
-
-        // Allow clicking only on EP01 for S01
-        if (season === 'S01' && episodeNum === '01') {
-            buttons += `<button class="episode-button" onclick="openEpisode('${season}', '${episodeNum}')">${season} EP${episodeNum}</button>`;
-        } else {
-            buttons += `<button class="episode-button disabled">${season} EP${episodeNum}</button>`;
-        }
+        // Create all episode buttons from EP01 to EP52
+        buttons += `<button class="episode-button" onclick="openEpisode('${season}', '${episodeNum}')">${season} EP${episodeNum}</button>`;
     }
     return buttons;
 }
 
 function openEpisode(season, episode) {
-    if (season === 'S01' && episode === '01') {
-        let query = `Doraemon ${season} Episode ${episode}`;
-        window.location.href = `https://www.google.com/search?q=${query}`;
-    }
+    // Open Google search URL for the selected season and episode
+    let query = `Doraemon ${season} Episode ${episode}`;
+    window.location.href = `https://www.google.com/search?q=${query}`;
 }
