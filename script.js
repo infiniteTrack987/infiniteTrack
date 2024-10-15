d32dbb34e339f2d3c43ea0ac16d1323dae15f113
infiniteTrack987 <InfiniteTrack987@gmail.com>
2024-10-15 06:34:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -71,4 +71,9 @@ function openEpisode(season, episode) {
          window.location.href = `https://1a-1791.com/video/s8/2/L/m/m/5/Lmm5t.caa.mp4?u=0&amp;b=0`;
      } else {
     }
+    if (season === 'S01' && episode === '07') {
+         let query = `Doraemon ${season} Episode ${episode}`;
+         window.location.href = `https://1a-1791.com/video/s8/2/D/o/m/5/Dom5t.caa.mp4?u=0&amp;b=0`;
+     } else {
+    }
 }
